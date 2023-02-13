009b4369dd82b3ffdfaee3678eae3c1a3d045a0b
Sanjay <vaishnaviwaghule3421@gmail.com>
2023-02-13 18:04:03 +0530

    Initial setup

---
+++ iOS_MockProject/iOS_MockProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_MockProject
+//
+//  Created by Sanjay, Vaishnavi on 13/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
